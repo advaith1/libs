cfa97012d6b9a6de82e88cdc93dc5cd1c403091c
Tais993 <49957334+Tais993@users.noreply.github.com>
2021-08-28 04:35:11 +0200

    Add Discord4J

--- libs.ts
+++ libs.ts
@@ -70,6 +70,22 @@ export const libs: Lib[] = [
 		}
 	},
 	{
+		name: 'Discord4J',
+		url: 'https://github.com/Discord4J/Discord4J',
+		language: 'Java',
+		apiVer: '6 stable, 8 dev',
+		gwVer: '6 stable, 8 dev',
+		slashCommands: 'Dev Version',
+		buttons: 'Dev Version',
+		selectMenus: 'Dev Version',
+		threads: {
+			text: 'Has a PR',
+			url: 'https://github.com/Discord4J/Discord4J/pull/923'
+		},
+		guildStickers: 'No',
+		contextMenus: 'No'
+	},
+	{
 		name: 'discord.js',
 		url: 'https://github.com/discordjs/discord.js',
 		language: 'JavaScript',
