87f0fcf05177374c120b5be08181a7cd5daced3a
Szymon Uglis <szymon@l7ssha.xyz>
2021-12-20 20:47:16 +0100

    nyxx: scheduled events are now in stable release (#47)

--- libs.ts
+++ libs.ts
@@ -438,7 +438,7 @@ export const libs: Lib[] = [
 		guildStickers: 'Yes',
 		contextMenus: 'Yes',
 		autocomplete: 'Yes',
-		scheduledEvents: 'Dev Version'
+		scheduledEvents: 'Yes'
 	},
 	{
 		name: 'discord.js',
