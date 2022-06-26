90c85719744e23a06652321c5da2da71bc6d9874
caneleex <me@mlnr.dev>
2022-06-26 17:33:31 +0200

    JDA and disgo have PRs for automod

--- libs.ts
+++ libs.ts
@@ -427,7 +427,10 @@ export const libs: Lib[] = [
 		timeouts: 'Yes',
 		modals: 'Yes',
 		permsv2: 'Yes',
-		automod: 'No',
+		automod: {
+			text: 'Has a PR',
+			url: 'https://github.com/disgoorg/disgo/pull/167'
+		},
 		localization: 'Yes'
 	},
 	{
@@ -608,7 +611,10 @@ export const libs: Lib[] = [
 			text: 'Has a PR',
 			url: 'https://github.com/DV8FromTheWorld/JDA/pull/2113'
 		},
-		automod: 'No',
+		automod: {
+			text: 'Has a PR',
+			url: 'https://github.com/DV8FromTheWorld/JDA/pull/2159'
+		},
 		localization: {
 			text: 'Has a PR',
 			url: 'https://github.com/DV8FromTheWorld/JDA/pull/2090'
