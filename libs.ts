099621119883743194c21d8619548084fc881e41
Sham <thesham@proton.me>
2022-07-13 18:41:52 +0200

    refactor(nextcord): change automod to new PR URL

--- libs.ts
+++ libs.ts
@@ -1126,7 +1126,7 @@ export const libs: Lib[] = [
 		permsv2: 'Yes',
 		automod: {
 			text: 'Has a PR',
-			url: 'https://github.com/nextcord/nextcord/pull/682'
+			url: 'https://github.com/nextcord/nextcord/pull/740'
 		},
 		localization: 'Yes'
 	},
