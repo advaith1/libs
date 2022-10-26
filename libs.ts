7f9bc578636241ff9868dd3f66b528d20b07476f
kubus <38988733+xiboon@users.noreply.github.com>
2022-10-26 17:32:45 +0200

    Update libs.ts
    Co-authored-by: Superchupu <53496941+SuperchupuDev@users.noreply.github.com>

--- libs.ts
+++ libs.ts
@@ -861,7 +861,10 @@ export const libs: Lib[] = [
 		scheduledEvents: 'No',
 		timeouts: 'Yes',
 		modals: 'Yes',
-		permsv2: {text: "Has a PR", url: "https://github.com/tiscordlib/tiscord/pull/84"},
+		permsv2: {
+			text: "Has a PR",
+			url: "https://github.com/tiscordlib/tiscord/pull/84"
+		},
 		automod: 'No',
 		localization: {
 			text: "Has a PR",
