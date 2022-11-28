29c20e4d96632d3f0fff07f7bd4633406e2a6d6f
BobDotCom <71356958+BobDotCom@users.noreply.github.com>
2022-11-28 17:23:46 -0600

    Pycord forum support is merged (#196)

--- libs.ts
+++ libs.ts
@@ -1209,10 +1209,7 @@ export const libs: Lib[] = [
 		permsv2: 'Yes',
 		automod: 'Yes',
 		localization: 'Yes',
-		forums: {
-			text: 'Has a PR',
-			url: 'https://github.com/Pycord-Development/pycord/pull/1636'
-		}
+		forums: 'Yes'
 	},
 	{
 		name: 'discorb',
