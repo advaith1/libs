5094d79b449de53a0f0bae5058c84cb482d72e5b
Soheab_ <33902984+Soheab@users.noreply.github.com>
2023-03-02 12:59:09 +0100

    discord.py has support for all select menus

--- libs.ts
+++ libs.ts
@@ -1042,7 +1042,7 @@ export const libs: Lib[] = [
 		gwVer: '10',
 		slashCommands: 'Yes',
 		buttons: 'Yes',
-		selectMenus: 'Partial',
+		selectMenus: 'Yes',
 		threads: 'Yes',
 		guildStickers: 'Yes',
 		contextMenus: 'Yes',
