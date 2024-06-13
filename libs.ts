9d83fdc0a91fce4f652287b2d1c776da36a5b7e1
Craig Edwards (Brain) <braindigitalis@users.noreply.github.com>
2024-06-13 14:44:01 +0100

    set dpp monetization support to full

--- libs.ts
+++ libs.ts
@@ -220,7 +220,7 @@ export const libs: Lib[] = [
 		automod: 'Yes',
 		localization: 'Yes',
 		forums: 'Yes',
-		monetization: 'Partial',
+		monetization: 'Yes',
 		userApps: 'No',
 		polls: 'Yes'
 	},
