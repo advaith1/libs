076d6ca4389a22023d02e473d7c85dd36683bf18
Craig Edwards (Brain) <braindigitalis@users.noreply.github.com>
2024-10-23 16:00:31 +0100

    Change user apps to 'master branch' in D++
    as above, master branch of D++ now has user apps support.

--- libs.ts
+++ libs.ts
@@ -234,7 +234,7 @@ export const libs: Lib[] = [
 		localization: 'Yes',
 		forums: 'Yes',
 		monetization: 'Yes',
-		userApps: 'No',
+		userApps: 'Master Branch',
 		polls: 'Yes',
 		forwarding: 'Yes',
 		appEmoji: 'Yes',
