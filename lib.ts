a4bd2a82420a16f992fbbc9b8a39349d3d0aab36
Craig Edwards (Brain) <braindigitalis@users.noreply.github.com>
2024-11-06 10:21:48 +0000

    Add support for voice version in table. Resolves #278 (#288)
    * basic structure to add type for voice gw version
    
    * libs voice support versions up to 'Go' lang
    
    * version 8 should probably be green at least for now
    
    * add remaining libs
    
    * Update libs.ts
    
    * fix: remove ? voiceVer
    
    * Update pages/index.tsx
    
    Co-authored-by: advaith <advaithj1@gmail.com>
    
    * remove '?' color
    
    ---------
    
    Co-authored-by: advaith <advaithj1@gmail.com>

--- lib.ts
+++ lib.ts
@@ -1,4 +1,5 @@
 type version = 6 | 7 | 8 | 9 | 10 | string
+type voiceVersion = "-" | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | "E2EE" | string
 export type LinkableString = string | { text: string; url: string }
 
 export interface Lib {
@@ -7,6 +8,7 @@ export interface Lib {
 	language: string
 	apiVer: version
 	gwVer: version
+	voiceVer: voiceVersion
 	slashCommands: LinkableString
 	buttons: LinkableString
 	selectMenus: LinkableString
