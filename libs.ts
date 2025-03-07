262a1bcdde641502c07eaa13e2d1ab548248e230
Lala Sabathil <aiko@aitsys.dev>
2025-03-07 14:58:51 +0100

    Update libs.ts

--- libs.ts
+++ libs.ts
@@ -835,10 +835,7 @@ export const libs: Lib[] = [
 		polls: 'Dev Version',
 		forwarding: 'No',
 		appEmoji: 'Dev Version',
-		componentsV2: {
-			text: 'Has a PR',
-			url: 'https://github.com/Discord4J/Discord4J/pull/1294'
-		}
+		componentsV2: 'Yes'
 	},
 	{
 		name: 'Javacord 🪦',
